6747c17638cd6bf047f340ee563bfb16ec942449
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2024-02-12 10:16:32 +0100

    Update firebaseconfig.ts

--- app/services/firebaseconfig.ts
+++ app/services/firebaseconfig.ts
@@ -28,23 +28,13 @@ import {
   getReactNativePersistence
 } from 'firebase/auth';
 
-// for some reason firestore lite doesn't read env variables, my mistake somewhere?
-/*const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FB_APP_ID,
-};*/
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDi9ybfSnATQ_7TNaDWyaHXhsqhae0-O_Q",
-  authDomain: "travel-snap-47abd.firebaseapp.com",
-  projectId: "travel-snap-47abd",
-  storageBucket: "travel-snap-47abd.appspot.com",
-  messagingSenderId: "603110860598",
-  appId: "1:603110860598:web:6852d31cf10ce98c017597"
 };
 
 const app = initializeApp(firebaseConfig);
